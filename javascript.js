c8f2217067a1f3c9e5ec2ada5f372eaa213cf950
FHP769 <Avelinaoneill@yahoo.com>
2021-04-27 21:44:11 +0800

    Update, json file

--- javascript.js
+++ javascript.js
@@ -1,36 +1,35 @@
-
+//Random number value
 var randomNumber;
-var bolLogin
 
+//login Bool
+var bolLogin = false;
+var nologinbanner = false;
+
+//Username info
 var firstName;
 var lastName;
 var email;
 var phoneNumber;
 
-
-var urlDatabase = [
-    "https://www.francebleu.fr/emissions/ca-va-dans-le-bon-sens-en-poitou/poitou/quel-est-l-impact-du-changement-climatique-sur-les-migrations-d-oiseaux-marins",
-    "https://www.terrafemina.com/article/environnement-pourquoi-2021-est-une-annee-cruciale-pour-agir_a357981/1",
-    "https://www.franceinter.fr/monde/rechauffement-climatique-2020-est-l-annee-la-plus-chaude-jamais-enregistree-en-europe",
-    "https://www.latribune.fr/entreprises-finance/transitions-ecologiques/la-planete-toujours-vers-un-rechauffement-de-3-c-malgre-la-pandemie-865456.html",
-    "https://www.lci.fr/environnement-ecologie/rechauffement-climatique-la-terre-ne-sera-pas-sauvee-sans-contribution-de-l-inde-2183958.html",
-    "https://vivredemain.fr/2021/03/26/le-rechauffement-climatique-est-catastrophique-en-russie/",
-    "https://www.consoglobe.com/rechauffement-climatique-fonte-iceberg-cg",
-    "https://www.futura-sciences.com/tech/actualites/technologie-visualisez-acceleration-rechauffement-climatique-google-earth-86843/",
-    "https://www.argusdelassurance.com/green-assurance/rechauffement-climatique/catastrophes-naturelles-une-annee-2020-tres-couteuse-pour-l-assurance-mondiale.180584"
-
-]
-
+// Charge onClick function, puis cherche le ficher Json pour ensuite convertir les Url en Random number
 function LoadRID(){
-   var randomNumber = Math.round(Math.random() * urlDatabase.length);
-   var randomUrl = urlDatabase[randomNumber];
-   window.location.href = randomUrl;
-   document.getElementById("RN").value=randomNumber;
-  //console.log(randomNumber)
-};
 
-function isLoged(){
+console.log('loading')
+fetch("Data/data.json").then(response => response.json())
+.then(data => {
+  let urlDatabase = data.urlDatabase;
 
+  if (bolLogin === true) {
+        var randomNumber = Math.round(Math.random() * urlDatabase.length);
+        var randomUrl = urlDatabase[randomNumber];
+        window.location.href = randomUrl;
+        document.getElementById("RN").value = randomNumber;
+        console.log('this')
+      }else {
+        nologinbanner = true
+      }
 
+})}
 
-}
+// Get adress
+
