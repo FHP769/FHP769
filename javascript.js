86ca102c15ccfd5e5b9e0a56a1604764a8e06f96
FHP769 <Avelinaoneill@yahoo.com>
2021-04-27 22:09:47 +0800

    little changes

--- javascript.js
+++ javascript.js
@@ -14,7 +14,6 @@ var phoneNumber;
 // Charge onClick function, puis cherche le ficher Json pour ensuite convertir les Url en Random number
 function LoadRID(){
 
-console.log('loading')
 fetch("Data/data.json").then(response => response.json())
 .then(data => {
   let urlDatabase = data.urlDatabase;
