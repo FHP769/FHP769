97f89fc0112ea43e47797458557f766a42744171
FHP769 <Avelinaoneill@yahoo.com>
2021-04-28 19:00:32 +0800

    Javascript css html update

--- javascript.js
+++ javascript.js
@@ -1,43 +1,45 @@
-//Random number value
+//Function pour les valeurs randoms
 var randomNumber;
-var test = 'test';
+var test = "test";
 
-//login Bool
+//Boolean pour les login
 var bolLogin = false;
 var nologinbanner = false;
 
-//Login + search
-function userQuery(){
-
+//Invocation de la function issue de l'event trigger 'OnClick' depuis le HTML
+// Recuperation des elements stockees dans les cases a username etc
+// Regarde si l'utilisateur a entree des donnees
+function userQuery() {
   //Username info
-  var firstName = document.getElementById('input-Prenom').value;
-  var lastName = document.getElementById('input-Nom').value;
-  var email = document.getElementById('input-email').value;
 
-  if (firstName !== '' && lastName !== '' && email !== '')
-  {
+  var firstName = document.getElementById("input-Prenom").value;
+  var lastName = document.getElementById("input-Nom").value;
+  var email = document.getElementById("input-email").value;
+
+  document.location.reload();
+
+  if (firstName !== "" && lastName !== "" && email !== "") {
     bolLogin = true;
     functionrandomUrl();
-  }
-  else
-  {
+  } else {
     nologinbanner = true;
-  
   }
 }
 
-// Charge onClick function, puis cherche le ficher Json pour ensuite convertir les Url en Random number
-function functionrandomUrl(){
+// Est invoquer depuis UserQuery si des informations sont entree
+// Cherche le ficher Json, le convertis en objet javascript
+// Utilisation de la function Math de javascript pour generer une url aleatoire
+function functionrandomUrl() {
+  fetch("Data/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      let urlDatabase = data.urlDatabase;
 
-fetch("Data/data.json").then(response => response.json())
-.then(data => {
-  let urlDatabase = data.urlDatabase;
-
-  if (bolLogin === true) {
+      if (bolLogin === true) {
         var randomNumber = Math.round(Math.random() * urlDatabase.length);
         var randomUrl = urlDatabase[randomNumber];
-        window.open(randomUrl, target="_blank")
+        window.open(randomUrl, (target = "_blank", rel="noopener noreferrer"));
         document.getElementById("RN").value = randomNumber;
       }
-})}
-
+    });
+}
---
+++ like_button.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  // class content
+  render(){
+    return (
+      <h1>Hello React!</h1>
+    )
+  }
+}
