6b242dba64c284db7c1459cd697931294238f69c
FHP769 <Avelinaoneill@yahoo.com>
2021-05-09 06:11:03 -1000

    Major changes, CSS,  IOS/Android support

--- javascript.js
+++ javascript.js
@@ -23,7 +23,13 @@ function functionrandomUrl() {
 
       var randomNumber = Math.round(Math.random() * urlDatabase.length);
       var randomUrl = urlDatabase[randomNumber];
-      window.open(randomUrl, (target = "_blank"));
+
+      if( /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        window.location.assign(randomUrl);
+       }
+       else if( /Chrome|Mozilla|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        window.open(randomUrl, (target = "_blank"))
+       };
     });
 }
 
@@ -35,16 +41,29 @@ function userInfo() {
   userQuery(userInput1, userInput2, userInput3);
 }
 
+
+ /* Message d'error pas login */
 function loginRequired() {
-  document.getElementById("popCo").style.display = "unset";
 
-  setInterval(cancel, 8550);
+  indexd = 8550;
+  document.getElementById('popCo').style.display = "unset";
 
+  if( /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    document.getElementById('popCo').style.position = "relative";
+    document.getElementById('popCo').style.height = "95%";
+    document.getElementById('popCo').style.width = "95%";
+    document.getElementById('popCo').style.justifyItems = "center";
+    document.getElementById('popCo').style.marginTop = "10%";
+    document.getElementById('popCo').style.backgroundColor= "rgba(231, 74, 26, 0.815)";
+   }
+/* Trigger cancel au bout de 8550s cancel le message */
+  setInterval(cancel, indexd);
   function cancel() {
     document.getElementById("popCo").style.display = "none";
+    window.location.reload()
   }
 }
-
+/* Trigger quand les informations box sont cliquer et cancel le message */
 let inputelem = document.getElementById("input-Prenom");
 let inputelem2 = document.getElementById("input-Nom");
 let inputelem3 = document.getElementById("input-email");
